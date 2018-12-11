00cc39558971d32595d409dd41f94f2753f6ce2c
saliha-oudjidane <35006951+saliha-oudjidane@users.noreply.github.com>
2018-12-11 01:40:15 +0100

    Update XmlContent.php

--- src/XmlContent.php
+++ src/XmlContent.php
@@ -12,43 +12,42 @@ class XmlContent implements RendererInterface
   {
 
 	return sprintf('$simpleXml = simplexml_load_file($media);
-	//----------------------------------------------------
+	
 	 echo "<h1><b>Rréférence :</h1></b>";
 	 echo $simpleXml->control->recordId;echo "<br>";
-	//---------------------------------------------------
+	
 	echo "<h1><b>Statut  :</h1></b>";
 	 echo $simpleXml->control->maintenanceStatus;echo "<br>";
-	 //--------------------------------------------------
+	 
 	 echo "<h1><b>Nom :</h1></b>";
 	 echo $simpleXml->control->maintenanceAgency->agencyName;echo "<br>";
-	//-------------------------------------------------
+	
 	echo "<h1><b>Langue :</h1></b>";
 	 echo $simpleXml->control->languageDeclaration->language;echo "<br>";
 
-	//------------------------------------------------
 	 echo "<h1><b>Citation :</h1></b>";
 	 echo $simpleXml->control->conventionDeclaration->citation;echo "<br>";
-	//-------------------------------------------------
+	
 	 echo "<h1><b>Date de création :</h1></b>";
 	 echo $simpleXml->control->maintenanceHistory->maintenanceEvent->eventDateTime;echo "<br>";
-	//------------------------------------------------
+	
 	 echo "<h1><b>Description :</h1></b>";
 	 echo $simpleXml->control->maintenanceHistory->maintenanceEvent[1]->eventDescription;echo "<br>";
-	//------------------------------------------------
+	
 	 echo "<h1><b>Source:</h1></b>";
 	 echo $simpleXml->control->sources->source->sourceEntry;echo "<br>";
-	//-----------------------------------------------
+
 
 	 echo "<h1><b>Partis:</h1></b>";
 	 echo $simpleXml->cpfDescription->identity->nameEntry->part;echo "<br>";
-	//--------------------------------------------------
+	
 
 	echo "<h1><b>Dates :</h1></b>";
 	echo $simpleXml->cpfDescription->description->existDates->dateRange->fromDate;
-	 //-----------------------------------------------
+	
 	echo "&nbsp <b> à </b>&nbsp";
 	echo $simpleXml->cpfDescription->description->existDates->dateRange->toDate;echo "<br>";
-	//-------------------------------------------------
+	
 
 	',$view->escapeHtml($media->originalUrl())
 
